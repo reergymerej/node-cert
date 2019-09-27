af972d7f668db4e290a9538d899287707f0f1e48
Jeremy Greer <jex.grizzle@gmail.com>
2019-09-27 11:51:48 -0700

    Test silly bases

--- projects/url-shortener/bll/id.js
+++ projects/url-shortener/bll/id.js
@@ -6,17 +6,34 @@ const values = CHARS.split('').reduce((acc, value, i) => {
   }
 }, {})
 
-const toBase62 = int => {
-  const BASE = CHARS.length
-  if (int < BASE) {
-    return values[int]
-  } else {
-    const left = toBase62(Math.floor(int / BASE))
-    const right = toBase62(int % BASE)
-    return `${left}${right}`
+
+const getIntEncoder = (series) => {
+  const values = series.split('').reduce((acc, value, i) => {
+    return {
+      ...acc,
+      [i]: value,
+    }
+  }, {})
+
+  const encoder = (int) => {
+    const base = series.length
+    if (int < base) {
+      return values[int]
+    } else {
+      const left = encoder(Math.floor(int / base))
+      const right = encoder(int % base)
+      return `${left}${right}`
+    }
   }
+
+  encoder.radix = series.length
+  encoder.series = series
+  return encoder
 }
+
+const toBase62 = getIntEncoder(CHARS)
 
 module.exports = {
   fromInt: toBase62,
+  getIntEncoder,
 }
--- projects/url-shortener/bll/id.test.js
+++ projects/url-shortener/bll/id.test.js
@@ -19,3 +19,55 @@ describe('id', () => {
     expect(id.fromInt(int)).toBe(expected)
   })
 })
+
+describe('getIntEncoder', () => {
+  it('should reveal its radix', () => {
+    const encoder = id.getIntEncoder('asdf')
+    expect(encoder.radix).toBe(4)
+  })
+
+  it('should reveal its series', () => {
+    const series = 'asdfqwer1234-xy'
+    const encoder = id.getIntEncoder(series)
+    expect(encoder.series).toBe(series)
+  })
+
+  it('should handle base62', () => {
+    const series = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const encoder = id.getIntEncoder(series)
+    expect(encoder.radix).toBe(62)
+    expect(encoder(620)).toBe('a0')
+  })
+
+  describe('012abc', () => {
+    const encoder = id.getIntEncoder('012abc')
+    it.each`
+      int | expected
+      ${0}  | ${'0'}
+      ${1}  | ${'1'}
+      ${2}  | ${'2'}
+      ${3}  | ${'a'}
+      ${4}  | ${'b'}
+      ${5}  | ${'c'}
+      ${6}  | ${'10'}
+    `('returns $expected for $int', ({ int, expected }) => {
+      expect(encoder(int)).toBe(expected)
+    })
+  })
+
+  describe('abc987', () => {
+    const encoder = id.getIntEncoder('abc987')
+    it.each`
+      int | expected
+      ${0}  | ${'a'}
+      ${1}  | ${'b'}
+      ${2}  | ${'c'}
+      ${3}  | ${'9'}
+      ${4}  | ${'8'}
+      ${5}  | ${'7'}
+      ${6}  | ${'ba'}
+    `('returns $expected for $int', ({ int, expected }) => {
+      expect(encoder(int)).toBe(expected)
+    })
+  })
+})
